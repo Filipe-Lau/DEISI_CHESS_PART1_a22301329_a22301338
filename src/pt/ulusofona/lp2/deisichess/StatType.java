6007a76c69e4c0367d5e595aff8a4cbd323f7975
Filipe Lau <filipemiguel1234@gmail.com>
2023-12-23 21:21:56 +0000

    PECAS_MAIS_5_CAPTURADAS para PECAS_MAIS_5_CAPTURAS

--- src/pt/ulusofona/lp2/deisichess/StatType.java
+++ src/pt/ulusofona/lp2/deisichess/StatType.java
@@ -3,7 +3,7 @@ package pt.ulusofona.lp2.deisichess;
 public enum StatType {
     TOP_5_CAPTURAS,
     TOP_5_PONTOS,
-    PECAS_MAIS_5_CAPTURADAS,
+    PECAS_MAIS_5_CAPTURAS,
     PECAS_MAIS_BARALHADAS,
     TIPOS_CAPTURADOS,
 }
